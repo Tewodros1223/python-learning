829d50adb9267210e6024f2ef3dcd060932dea15
Teddy <dagim.1408@gmail.com>
2026-03-23 12:02:20 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
 y = 24
 x = f"my name is tewodros Kassa 'i am {y} years old'"
+
 print(x.isalpha())
+#boolean 
+z = 30
+if z > y:
+    print ("30 is greater than 24 ")
+else:
+    print ("display the error") 
+#operators
+#arthimethic operator
+x, y = 2, 3  
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+print(x % y)
+print(x // y)
+ #assignment operator
+print(x:=5)
+ #list display by loop 
+thislist = ["orage", "banana", "apple", "mango"]
+m = thislist[0]
+n = thislist[1]
+o = thislist[2]
+p = thislist[3]
+print(m, n, o, p)
+for i in range(len(thislist)):
+    print(thislist[i])
+#dictionary
+fruits = {"banana" : "yellow",
+        "apple" : "red",
+        "mango" : "orange"}
+print (fruits.values())  
+print(fruits["banana"])  
